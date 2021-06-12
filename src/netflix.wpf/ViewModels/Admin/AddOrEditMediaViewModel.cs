ba56f71008e7bb3544d49ccb608896762f38a655
Dinh Truong <43572251+DinhTruong98@users.noreply.github.com>
2021-06-12 10:47:30 +0700

    add/edit media

--- src/netflix.wpf/ViewModels/Admin/AddOrEditMediaViewModel.cs
+++ src/netflix.wpf/ViewModels/Admin/AddOrEditMediaViewModel.cs
@@ -15,6 +15,9 @@ namespace netflix.wpf.ViewModels.Admin
         private Media media;
         public bool IsAdd { get; set; }
         public string PageTitle { get; set; }
+        private ICommand saveChange;
+        private ICommand selectFile;
+        public string fileName;
         public Media Media
         {
             get => media;
@@ -24,8 +27,15 @@ namespace netflix.wpf.ViewModels.Admin
                 OnPropertyChanged();
             }
         }
-        private ICommand saveChange;
-
+        public string FileName
+        {
+            get => fileName;
+            set
+            {
+                fileName = value;
+                OnPropertyChanged();
+            }
+        }
         public ICommand SaveChange
         {
             get
@@ -36,6 +46,38 @@ namespace netflix.wpf.ViewModels.Admin
                        p => true, p => saveToDb());
                 }
                 return saveChange;
+            }
+        }
+        public ICommand SelectFile
+        {
+            get
+            {
+                if (selectFile == null)
+                {
+                    selectFile = new RelayCommand(
+                       p => true, p => getFile());
+                }
+                return selectFile;
+            }
+        }
+
+        private void getFile()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".mp4";
+            dlg.Filter = "Files|*.mp4";
+            // Display OpenFileDialog by calling ShowDialog method 
+            bool? result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                FileName = filename;
             }
         }
 
@@ -56,6 +98,7 @@ namespace netflix.wpf.ViewModels.Admin
         public AddOrEditMediaViewModel(Media _media)
         {
             Media = _media;
+            FileName = "Chưa file nào được chọn";
             if (_media is null)
             {
                 IsAdd = true;
